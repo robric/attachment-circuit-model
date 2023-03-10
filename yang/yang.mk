516b472ddcaeb539f2f0a6cba6165ebdca158ff7
Med <mohamed.boucadair@orange.com>
2023-03-10 13:28:45 +0100

    Update yang.mk

--- yang/yang.mk
+++ yang/yang.mk
@@ -12,14 +12,12 @@ YANG_PATH="$(YANGDIR):$(STDYANGDIR)/standard/ietf/RFC/:$(STDYANGDIR)/standard/ie
 endif
 YANG=$(wildcard $(YANGDIR)/*.yang)
 STDYANG=$(wildcard $(YANGDIR)/ietf-*.yang)
-EXPYANG=$(wildcard $(YANGDIR)/example-*.yang)
 TXT=$(patsubst $(YANGDIR)/%.yang,%-diagram.txt,$(YANG))
 
 .PHONY: yang-lint yang-gen-diagram yang-clean
 
-pyang-lint: $(STDYANG) $(EXPYANG) $(STDYANGDIR)
-	pyang -V --ietf -p $(YANG_PATH) $(STDYANG)
-	pyang -V -p $(YANG_PATH) $(EXPYANG)
+pyang-lint: $(STDYANG) $(STDYANGDIR)
+	pyang -V --ietf -p $(YANG_PATH) $(STDYANG)	
 
 yang-lint: $(STDYANG) $(STDYANGDIR)
 	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules -p $(STDYANGDIR)/standard/ieee/published/802.1/ $(STDYANG) -i
